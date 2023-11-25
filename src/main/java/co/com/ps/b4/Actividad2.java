094cdeddd3b2356e0097973a046bcce85c769081
“mmg114” <mmg.usb@gmail.com>
2023-11-24 21:11:14 -0500

    Actualizacion de actividades pendientes

--- src/main/java/co/com/ps/b4/Actividad2.java
+++ src/main/java/co/com/ps/b4/Actividad2.java
@@ -1,5 +1,6 @@
 package co.com.ps.b4;
 
+//Crear objeto mascota con minimo 3 atributos y su constructor, y llamarlo desde un metodo main
 public class Actividad2 {
     public static void main(String[] args) {
 
---
+++ src/main/java/co/com/ps/b5/Actividad3.java
@@ -0,0 +1,7 @@
+package co.com.ps.b5;
+
+
+// decidir si un numero es negativo positivo o 0, clacular los 10 primero pares
+// Adional, dar un limite mayor y menor y encontrar los numero primos
+public class Actividad3 {
+}
