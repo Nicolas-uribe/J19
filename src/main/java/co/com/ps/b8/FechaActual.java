ac4faf432d290a87f636c5e46981d308d189a224
Nicolas-uribe <nicolas.uribe.trejos@gmail.com>
2023-11-30 19:48:14 -0500

    Última clase Java :(  DATE AND CALENDAR

---
+++ src/main/java/co/com/ps/b8/FechaActual.java
@@ -0,0 +1,10 @@
+package co.com.ps.b8;
+
+import java.util.Date;
+
+public class FechaActual {
+    public static void main(String[] args) {
+        Date fechaActual = new Date();
+        System.out.println("Fecha actual: " + fechaActual);
+    }
+}
